31d405371f194980136f830f6ebf2a4fba13dade
Oleg Klyushnik <medvezhatneg@gmail.com>
2022-07-12 10:33:26 -0700

    image working in details screen

--- app/src/main/java/studio/sanguine/codingtest/MovieDetailsActivity.kt
+++ app/src/main/java/studio/sanguine/codingtest/MovieDetailsActivity.kt
@@ -2,10 +2,13 @@ package studio.sanguine.codingtest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 import android.widget.TextView
 import org.w3c.dom.Text
 import studio.sanguine.codingtest.R
 import studio.sanguine.codingtest.models.Movie
+import studio.sanguine.codingtest.repositories.Repository
+import studio.sanguine.codingtest.viewmodels.MovieDetailsViewModel
 
 class MovieDetailsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,14 +18,24 @@ class MovieDetailsActivity : AppCompatActivity() {
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         val movie = intent.getSerializableExtra("movie")
 
+        val vm = MovieDetailsViewModel(Repository(App.createRetrofit()))
+
         val quoteTextView = findViewById<TextView>(R.id.details_quoteTextView)
         val actorNameTextView = findViewById<TextView>(R.id.details_actorNameTextView)
+        val imageView = findViewById<ImageView>(R.id.details_imageView)
+        var url = ""
 
         movie?.let{
             quoteTextView.setText((it as Movie).content)
             actorNameTextView.setText((it as Movie).actorName)
+            url = (it as Movie).imagethumburl ?: "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcSqoqCXj79wy81IYW05vx6FPlsc9RIeBAeTKR37QKwRoFOaoxtBL0P8RR8"
         }
 
+        vm.bitmap.observe(this){
+            imageView.setImageBitmap(it)
+        }
+
+        vm.getImage(url)
     }
 
     override fun onSupportNavigateUp(): Boolean {
--- app/src/main/java/studio/sanguine/codingtest/viewmodels/MovieDetailsViewModel.kt
+++ app/src/main/java/studio/sanguine/codingtest/viewmodels/MovieDetailsViewModel.kt
@@ -1,6 +1,8 @@
 package studio.sanguine.codingtest.viewmodels
 
 import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import okhttp3.ResponseBody
 import retrofit2.Call
@@ -9,23 +11,32 @@ import retrofit2.Response
 import studio.sanguine.codingtest.repositories.Repository
 
 class MovieDetailsViewModel(val repository: Repository) : ViewModel() {
-    lateinit var bitmap: Bitmap
-    val callback = MyCallback()
+    lateinit var bitmap: MutableLiveData<Bitmap>
+    init {
+        bitmap = MutableLiveData()
+    }
+
+    val getBitmap = fun(bmp: Bitmap){
+        bitmap.postValue(bmp)
+    }
+    val callback = MyCallback(getBitmap)
 
     fun getImage(url: String){
         val call : Call<ResponseBody> = repository.inter.fetchImage(url)
 
         call.enqueue(callback)
 
-
     }
+
 }
 
-class MyCallback : Callback<ResponseBody> {
+class MyCallback(val getBitmap : (Bitmap) -> Unit) : Callback<ResponseBody> {
 
     lateinit var bitmap: Bitmap
-    override fun onResponse(call: Call<ResponseBody>, response: Response<ResponseBody>) {
 
+    override fun onResponse(call: Call<ResponseBody>, response: Response<ResponseBody>) {
+        bitmap = BitmapFactory.decodeStream(response.body()?.byteStream())
+        getBitmap(bitmap)
     }
 
     override fun onFailure(call: Call<ResponseBody>, t: Throwable) {
